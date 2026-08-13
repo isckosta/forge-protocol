7a72e583f68b9722511ce48bc88e2d6a872a288d
Israel Costa <iscanalista@gmail.com>
2026-08-13 02:43:51 -0300

    refactor: centralize Forge version metadata

---
+++ src/forge_cli/version.py
@@ -0,0 +1,6 @@
+"""Forge CLI and Protocol version metadata."""
+
+CLI_VERSION = "0.1.0.dev0"
+PROTOCOL_ID = 1
+PROTOCOL_DISPLAY_VERSION = "1-draft"
+SUPPORTED_PROTOCOL_IDS = frozenset({PROTOCOL_ID})
